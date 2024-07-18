d35c1a969faddd1290beb1574897103d7c44209b
rlaphoenix <17136956+rlaphoenix@users.noreply.github.com>
2024-07-19 00:58:45 +0100

    Improve docs config

--- docs/conf.py
+++ docs/conf.py
@@ -1,59 +1,61 @@
 # Configuration file for the Sphinx documentation builder.
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+
 from datetime import datetime
 
 from dunamai import Style, Version
 
 # -- Project information -----------------------------------------------------
 
-project = 'pydvdcss'
-copyright = f'2021-{datetime.now().year}, rlaphoenix'
-author = 'rlaphoenix'
-
+project = "pydvdcss"
+copyright = f"2021-{datetime.now().year}, rlaphoenix"
+author = "rlaphoenix"
 version = Version.from_git().serialize(style=Style.SemVer)
 release = Version.from_git().base
 
 # -- General configuration ---------------------------------------------------
 
 extensions = [
-    'sphinx.ext.autosectionlabel',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'myst_parser'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.napoleon",
+    "myst_parser",
 ]
-
-master_doc = 'index'
-
 exclude_patterns = [
-    '_build',
-    'Thumbs.db',
-    '.DS_Store',
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+]
+root_doc = "index"
+templates_path = [
+    "_templates",
 ]
 
-templates_path = ['_templates']
+# -- Builder options ---------------------------------------------------------
 
-# -- Options for internationalization ----------------------------------------
-
-language = 'en'
-
-# -- Options for HTML output -------------------------------------------------
-
-html_theme = 'furo'
-html_static_path = ['_static']
-
+html_theme = "furo"
 html_css_files = [
-    'styles/custom.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/solid.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/brands.min.css',
+    "styles/custom.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/solid.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/brands.min.css",
 ]
-
+html_static_path = [
+    "_static",
+]
 html_sidebars = {
-    '**': [
-        'sidebar/scroll-start.html',
-        'sidebar/brand.html',
-        'sidebar/search.html',
-        'sidebar/navigation.html',
-        'sidebar/scroll-end.html',
-    ]
+    "**": [
+        "sidebar/scroll-start.html",
+        "sidebar/brand.html",
+        "sidebar/search.html",
+        "sidebar/navigation.html",
+        "sidebar/scroll-end.html",
+    ],
 }
+
+# -- MyST configuration ------------------------------------------------------
+
+myst_enable_extensions = [
+    "smartquotes",
+    "replacements",
+]
