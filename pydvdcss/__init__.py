18877a8d2fdda86f5bc89deca47447800b51bac6
rlaphoenix <17136956+rlaphoenix@users.noreply.github.com>
2026-06-21 22:18:58 +0100

    Expose enums/flags from package root

--- pydvdcss/__init__.py
+++ pydvdcss/__init__.py
@@ -1,1 +1,27 @@
-from pydvdcss.dvdcss import DvdCss  # noqa: F401
+from pydvdcss.dvdcss import DvdCss
+from pydvdcss.exceptions import (
+    AlreadyInUseError,
+    CloseError,
+    LibraryNotFoundError,
+    NoDeviceError,
+    OpenFailureError,
+    PyDvdCssError,
+    ReadError,
+    SeekError,
+)
+from pydvdcss.structs import DvdCssStreamCb, ReadFlag, SeekFlag
+
+__all__ = (
+    "AlreadyInUseError",
+    "CloseError",
+    "DvdCss",
+    "DvdCssStreamCb",
+    "LibraryNotFoundError",
+    "NoDeviceError",
+    "OpenFailureError",
+    "PyDvdCssError",
+    "ReadError",
+    "ReadFlag",
+    "SeekError",
+    "SeekFlag",
+)
